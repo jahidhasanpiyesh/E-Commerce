cd5605528432261a94b4e3680e72a26773c0b0cc
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-04-28 09:42:59 +0600

    initial commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # -------------I will created more app -------------
+    'coreapp',
+    'fashionapp',
 ]
 
 MIDDLEWARE = [
